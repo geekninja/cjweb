f0c962af5a27b456127864e52566f9d8d6c00504
elton silva <elton.chrls@gmail.com>
2016-09-23 16:21:32 -0300

    ok

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :author, -> {order(:name)}
 
+  default_scope { order('created_at DESC')}
+
   scope :by_author, -> (author_id) { where(author_id: author_id)}
   scope :by_title,   -> (title)      { where("title ILIKE '%#{title}%'")}
 
